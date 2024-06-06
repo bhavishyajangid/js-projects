745c1e5cb853fec89856b932b08b7bb39204d4bb
bhavishya8824@gmail.com <bhavishya8824@gmail.com>
2024-06-06 22:40:43 +0530

    final commit

--- script.js
+++ script.js
@@ -1,81 +1,115 @@
-let parentDiv = document.querySelector(".all-buttons")
-let input = document.querySelector("#input")
-let arr = [ 65 ,67  , 37 ,247 , 55 , 56 , 57 , 215 , 52 , 53 , 54 , 45 , 49 , 50 , 51 , 43 , 48,  77 , 46 , 61]
-let num = 0
+let parentDiv = document.querySelector(".all-buttons");
+let input = document.querySelector("#input");
+let arr = [
+     65, 67, 37, 247, 55, 56, 57, 215, 52, 53, 54, 45, 49, 50, 51, 43, 48, 77, 46,
+     61,
+];
+let num = 0;
 
-let recentvalue = ''
-let symbol = ''
-
-function rough(letter , clickedLetter){
-     console.log(clickedLetter);
-     if(clickedLetter === '='){
-          recentvalue += ""
-          
-     }else{
-          recentvalue += clickedLetter
+let recentvalue = "";
+let symbol = "";
+// function to check the clicked words are number or not 
+function rough(letter, clickedLetter) {
+     if (
+          clickedLetter === "=" ||
+          clickedLetter === "A" ||
+          clickedLetter === "C" ||
+          clickedLetter === "M"
+     ) {
+          recentvalue += "";
+          // symbol = "";
+     } else {
+          recentvalue += clickedLetter;
      }
 
-    
-     
-
-     
-     
-     
-     if(clickedLetter === "×" ||  clickedLetter === "+" || clickedLetter === "÷" ||  clickedLetter === "%" ||  clickedLetter === "-" ||  clickedLetter === "." ){
-          
-          symbol = clickedLetter
-          
+     if (
+          clickedLetter === "×" ||
+          clickedLetter === "+" ||
+          clickedLetter === "÷" ||
+          clickedLetter === "%" ||
+          clickedLetter === "-" ||
+          clickedLetter === "."
+     ) {
+          symbol = clickedLetter;
      }
-     
-     console.log(recentvalue);
 
-     input.value = recentvalue
-       
+     input.value = recentvalue;
 }
 
-function makingBtn(){
+function makingBtn() {
+     // make all the button using ascii code 
      for (let i = 0; i < arr.length; i++) {
-          let button = document.createElement("button")
-          button.textContent = String.fromCharCode(arr[i])
-          parentDiv.appendChild(button)
-          
-          button.addEventListener("click" , function(e){
-               
-               return rough(e.target , String.fromCharCode(arr[i]))
-          })
-          
+          let button = document.createElement("button");
+          button.textContent = String.fromCharCode(arr[i]);
+          parentDiv.appendChild(button);
+
+          button.addEventListener("click", function (e) {
+               return rough(e.target, String.fromCharCode(arr[i]));
+          });
      }
-     
-     
-     let allbtn = parentDiv.querySelectorAll("button")
-     allbtn.forEach((btn , i , arr) => {
-          btn.classList.add(`zero${num}`)
-          num++
-          })
+      // all class to all the buttons
+     let allbtn = parentDiv.querySelectorAll("button");
+     allbtn.forEach((btn, i, arr) => {
+          btn.classList.add(`zero${num}`);
+          num++;
+     });
 
-          let equalBtn = parentDiv.querySelector(".zero19");
-
-          equalBtn.addEventListener("click" , function(){
-               console.log(symbol);
-               let newnum =   recentvalue.split(symbol)
-               newnum.forEach((e , i , arr)=>{
-                 let num1 = parseInt(e)
-
-                 if(symbol === "+"){
-                    let result = num1 + num1
+     // target equal btn and add its funtionality 
+     let equalBtn = parentDiv.querySelector(".zero19");
+     equalBtn.addEventListener("click", function () {
+          
+          let newnum = recentvalue.split(/[- + % × ÷]/);
+          console.log(newnum);
+          newnum.forEach((e, i, arr) => {
+               let num1 = parseInt(arr[0]);
+               let num2 = parseInt(arr[1]);
+               console.log(num1 , num2);
+               if (symbol === "+") {
+                    let result = num1 + num2;
                     console.log(result);
-                 }
-               })
-          })
-
-
-
-   
-   
-
+                    input.value = result;
+                    recentvalue = result
+               }else if(symbol === "-"){
+                    let result = num1 - num2;
+                    input.value = result;
+                    recentvalue = result
+               }else if(symbol === "%"){
+                    let result = num1 % num2;
+                    input.value = result;
+                    recentvalue = result
+               }else if(symbol === "×"){
+                    let result = num1 * num2;
+                    input.value = result;
+                    recentvalue = result
+               }else if(symbol === "÷"){
+                    let result = num1 / num2;
+                    input.value = result;
+                    recentvalue = result
+               }
+          });
+     });
 }
 
-makingBtn()
+makingBtn();
+  
+//make clear all the digit btn from the input 
+function clearAllData() {
+     let clearBtn = document.querySelector(".zero0");
+     clearBtn.addEventListener("click", function () {
+          recentvalue = "";
+          input.value = "";
+     });
+}
+
+clearAllData();
 
 
+//make clear only one digit btn  from the input 
+function clearSingleData() {
+     let clear = document.querySelector(".zero1");
+     clear.addEventListener("click", function () {
+          recentvalue = recentvalue.slice(0, -1);
+     });
+}
 
+clearSingleData();
